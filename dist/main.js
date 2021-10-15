14960cc390c8199d2d42442f9e5dab32ae2ce57f
DanStevensonCO <danstevensonco@gmail.com>
2021-10-15 14:29:12 +0100

    add readonly error exampl

--- dist/main.js
+++ dist/main.js
@@ -2,7 +2,12 @@ var User = /** @class */ (function () {
     function User(firstName, lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.unchangeableName = firstName;
     }
+    // This errors because readonly is set above - cannot assign
+    // changeUnchangeableName(): void {
+    //     this.unchangeableName = "foo"
+    // }
     User.prototype.getFullName = function () {
         return this.firstName + " " + this.lastName;
     };

516bab53ee35431fffb380cd27fadca43a7589ef
DanStevensonCO <danstevensonco@gmail.com>
2021-10-17 21:14:37 +0100

    add generics example

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,30 @@
-interface UserInterface {
-    getFullName(): string
-}
-
-class User implements UserInterface {
-    firstName: string
-    lastName: string
-    readonly unchangeableName: string
-    static readonly maxAge = 50
-
-    constructor(firstName: string, lastName: string) {
-        this.firstName = firstName
-        this.lastName = lastName
-        this.unchangeableName = firstName
-    }
-
-    // "implements" makes this function required, since it is in the interface
-    getFullName(): string {
-        return this.firstName + " " + this.lastName
+const addId = <T extends object>(obj: T) => {
+    const id = Math.random().toString(16)
+    return {
+        ...obj,
+        id
     }
 }
 
-class Admin extends User {
-    private editor: string
+interface UserInterface<T> {
+    name: string
+    data: T
+}
 
-    setEditor(editor: string): void {
-        this.editor = editor
-    }
-
-    getEditor(): string {
-        return this.editor
+const user: UserInterface<{ meta: string }> = {
+    name: "Jack",
+    data: {
+        meta: "foo"
     }
 }
 
-const user = new User("Dan", "Stevenson")
-console.log(user.getFullName())
+const user2: UserInterface<string[]> = {
+    name: "John",
+    data: ["foo", "bar"]
+}
 
-const admin = new Admin("Foo", "Bar")
-console.log(admin.setEditor("New editor"))
+const result = addId(user)
+console.log("result", result)
+
+const result2 = addId(user2)
+console.log("result2", result2)
